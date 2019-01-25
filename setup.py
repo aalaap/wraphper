7ea68a286cdac3d155cfeda41ac816b1353f6161
Aalaap Ghag <aalaap@gmail.com>
2019-01-25 19:07:42 +0530

    Update setup.py to use module version number

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+from wraphper import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="wraphper",
-    version="0.0.1",
+    version=__version__,
     author="Aalaap Ghag",
     description="Python wrappers for PHP functions, when you just can't shake it.",
     long_description=long_description,
@@ -14,6 +15,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: PHP",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
