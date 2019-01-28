e5b0f7db3d71b70cb5669ba049a3511715e6e477
Aalaap Ghag <aalaap@gmail.com>
2019-01-28 17:39:51 +0530

    Add more tests

--- tests.py
+++ tests.py
@@ -1,12 +1,17 @@
 import unittest
-from wraphper import wraphper as php
+import wraphper as php
 
 class PythonPHPTestCase(unittest.TestCase):
     """Test cases for the wraphper module"""
 
     def test_count(self):
-        c = php.count([1, 2, 3])
-        self.assertEqual(c, 3)
+        test_list = [1, 2, 3]
+        test_dict = {1:'value','key':2}
+        test_tuple = (1, 2, 3, 4)
+
+        self.assertEqual(php.count(test_list), 3)
+        self.assertEqual(php.count(test_dict), 2)
+        self.assertEqual(php.count(test_tuple), 4)
 
 if __name__ == '__main__':
     unittest.main()
